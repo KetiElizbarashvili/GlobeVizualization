be6934085b1868681ea2b731b6664ba5982cfd87
Keti ELizbarashvili <99737397+KetiElizbarashvili@users.noreply.github.com>
2023-11-05 04:21:03 +0400

    three js boilerplate

---
+++ script.js
@@ -0,0 +1,56 @@
+var camera   = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1500),
+    scene    = new THREE.Scene(),
+    renderer = new THREE.WebGLRenderer({ antialias: true }),
+    geometry = new THREE.Geometry(),
+    mat      = new THREE.ParticleBasicMaterial({ color: 0x22aaff, size: 3 }),
+    radius   = 500;
+
+// Setup
+camera.position.z = 1500;
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+
+var geoToCartesian = function(lat, lon, radius) {
+  var x, y, z;
+  lat *= Math.PI / 180;
+  lon *= Math.PI / 180;
+  x = -radius * Math.cos(lat) * Math.cos(lon);
+  y = radius * Math.sin(lat);
+  z = radius * Math.cos(lat) * Math.sin(lon);
+  return new THREE.Vector3(x, y, z);
+};
+
+cities.forEach(function(city) {
+  var vertex = geoToCartesian(city[0], city[1], 400);
+  
+  geometry.vertices.push(vertex);
+
+  var factor = 100 + Math.random() * 1000;
+  var growth = Math.min(0.9, 0.35 + Math.random());
+  
+  requestAnimationFrame(function update() {
+      factor *= growth;
+      if (factor > 1) requestAnimationFrame(update);
+      vertex.normalize().multiplyScalar(radius + factor);
+  });    
+});
+
+var particles = new THREE.ParticleSystem(geometry, mat);
+
+scene.add(particles);
+
+
+requestAnimationFrame(function update() {
+  requestAnimationFrame(update);
+    
+  var time = Date.now() * 0.00005;
+  var hue = ( 360 * ( 1.0 + time ) % 360 ) / 360;
+
+  mat.color.setHSL(hue, 0.5, 0.5 );    
+  particles.rotation.y += 0.007;
+  particles.geometry.verticesNeedUpdate = true;
+    
+  renderer.render(scene, camera);
+}); 
